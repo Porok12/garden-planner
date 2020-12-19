7defdc698e933ad2881e2c77970765a4bb144400
Przemyslaw Papla <przemekpapla@gmail.com>
2020-12-19 16:40:05 +0100

    Add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar';
+import {Button as ButtonBootStrap} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar/>
+            <br/>
+
+            <div className="custom-control custom-switch">
+                <input type="checkbox" className="custom-control-input" id="customSwitch1"/>
+                <label className="custom-control-label" htmlFor="customSwitch1">Toggle this switch element</label>
+            </div>
+
+            <div className="custom-control custom-checkbox">
+                <input type="checkbox" className="custom-control-input" id="customCheck1"/>
+                <label className="custom-control-label" htmlFor="customCheck1">Check this custom checkbox</label>
+            </div>
+
+            <ButtonBootStrap>btn2</ButtonBootStrap>
+            <div className="form-outline">
+                <input type="text" id="form1" className="form-control"/>
+                <label className="form-label" htmlFor="form1">Example label</label>
+            </div>
+
+            <header className="App-header">
+                App header
+            </header>
+        </div>
+    );
 }
 
 export default App;
