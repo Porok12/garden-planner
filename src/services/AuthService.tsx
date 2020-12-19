bc94db315bebf31fd181987889a5f34c149a91e6
Przemysław Papla <przemekpapla@gmail.com>
2020-12-19 23:47:08 +0100

    Server structure

---
+++ src/services/AuthService.tsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/api/auth/';
+
+class AuthService {
+    login(username: string, password: string): void {
+        return axios
+            .post(API_URL + "signin", {
+                username,
+                password
+            })
+            .then((response: any) => {
+                if (response.data.accessToken) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
+
+                return response.data;
+            });
+    }
+
+    logout(): void {
+        localStorage.removeItem("user");
+    }
+
+    register(username: string, email: string, password: string): void {
+        return axios.post(API_URL + "signup", {
+            username,
+            email,
+            password
+        });
+    }
+
+    getCurrentUser(): void {
+        return JSON.parse(localStorage.getItem('user'));;
+    }
+}
+
+export default new AuthService();
