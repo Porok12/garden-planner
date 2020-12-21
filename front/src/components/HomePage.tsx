f9ced01dd88490a4bce9905961776f367ac115da
Przemysław Papla <przemekpapla@gmail.com>
2020-12-21 11:08:07 +0100

    HomePage

--- front/src/components/HomePage.tsx
+++ front/src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Form} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import {Canvas} from "react-three-fiber";
 import Box from "./Box";
 import { request, gql } from 'graphql-request';
@@ -18,38 +18,17 @@ class HomePage extends Component<any, any> {
 
     render() {
         return <>
-            <br/>
-            <Form style={{width: '300px', margin: 'auto'}}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+            <Container id="container" fluid>
+                <br/>
+                <Button onClick={this.graphql}>GraphQL</Button>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Switch type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-
-            <Button onClick={this.graphql}>GraphQL</Button>
-
-            <header className="App-header">
-                <Canvas>
+                <Canvas id="canvas">
                     <ambientLight />
                     <pointLight position={[10, 10, 10]} />
                     <Box position={[-1.2, 0, 0]} />
                     <Box position={[1.2, 0, 0]} />
                 </Canvas>
-            </header>
+            </Container>
         </>;
     }
 }
