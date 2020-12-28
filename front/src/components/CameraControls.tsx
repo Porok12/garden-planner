76edfdd8eb5949a704d2bd325e0946fa83ee1114
Przemysław Papla <przemekpapla@gmail.com>
2020-12-28 10:40:38 +0100

    CameraControls add invalidating

--- front/src/components/CameraControls.tsx
+++ front/src/components/CameraControls.tsx
@@ -1,17 +1,26 @@
-import React, {Component, useRef} from 'react';
+import React, {Component, useEffect, useRef} from 'react';
 import {extend, useFrame, useThree} from 'react-three-fiber';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {OrbitControls, MapControls} from "three/examples/jsm/controls/OrbitControls";
+import { Lensflare, LensflareElement } from 'three/examples/jsm/objects/Lensflare.js';
 // Extend will make OrbitControls available as a JSX element called orbitControls for us to use.
 extend({ OrbitControls });
+extend({ MapControls });
 
 function CameraControls(props: any) {
-    const { camera, gl: { domElement } } = useThree();
+    const { camera, gl: { domElement }, invalidate } = useThree();
 
-    const ref: any = useRef<OrbitControls>();
+    //React.RefObject<OrbitControls>
+    const ref: any = useRef<MapControls>();
     useFrame(() => ref.current.update());
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.removeEventListener('change', invalidate);
+            ref.current.addEventListener('change', invalidate);
+        }
+    }, [ref.current]);
     // @ts-ignore
-    return <orbitControls ref={ref} args={[camera, domElement]} />
-
+    return <orbitControls ref={ref} args={[camera, domElement]} maxPolarAngle={Math.PI/1.9} enableDamping />
+    // return <mapControls ref={ref} args={[camera, domElement]} maxPolarAngle={Math.PI/1.9} />;
 }
 
 export default CameraControls;
