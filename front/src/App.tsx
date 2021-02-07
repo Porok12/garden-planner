65ce52396f81b6631b321a923db4284abf66833c
Przemysław Papla <przemekpapla@gmail.com>
2021-02-07 20:28:55 +0100

    Bigger footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import Routing from "./components/Routing";
 import NavBar from "./components/NavBar";
 import {default as Footer} from "./components/Footer";
-import {QueryBuilder} from "react-advanced-search-builder";
 import LogoutManager from "./components/LogoutManager";
 
 function App() {
     return (
         <div className="App">
-            <QueryBuilder text="123"/>
             <Routing>
                 <NavBar/>
                 <LogoutManager />
--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,9 +1,39 @@
+import React from "react";
 import {Component} from "react";
+import {Container, Row, Col} from "react-bootstrap";
 
 class Foot extends Component {
     render() {
         return <footer id="footer">
-            Footer
+            <Container>
+                <Row className="pt-4">
+                    <Col>
+                        <h4>Docs</h4>
+                        <ul>
+                            <li>#1</li>
+                            <li>#2</li>
+                            <li>#3</li>
+                            <li>#4</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <h4>Community</h4>
+                        <li>#1</li>
+                        <li>#2</li>
+                    </Col>
+                    <Col>
+                        <h4>Socials</h4>
+                        <li>#1</li>
+                        <li>#2</li>
+                        <li>#3</li>
+                    </Col>
+                </Row>
+                <Row className="py-2">
+                    <Col className="text-center">
+                        Copyright &copy; 2021 Garden Planner.
+                    </Col>
+                </Row>
+            </Container>
         </footer>;
     }
 }
