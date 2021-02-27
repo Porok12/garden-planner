2cb7b7f202bbd3cf06ed6252981ea4c696becad9
Przemysław Papla <przemekpapla@gmail.com>
2021-02-27 19:47:47 +0100

    project model/entity

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 const config = require("../config/db.config.js");
+const pushData = require('./test.db');
 
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
@@ -32,85 +33,22 @@ db.sequelize = sequelize;
 db.user = require("../models/user")(sequelize, Sequelize);
 db.role = require("../models/role")(sequelize, Sequelize);
 db.plant = require("../models/plant")(sequelize, Sequelize);
+db.project = require("../models/project")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, { through: "user_roles" });
 db.user.belongsToMany(db.role, { through: "user_roles" });
+db.user.hasMany(db.project, { foreignKey: { allowNull: false } });
+db.project.belongsTo(db.user);
 
 db.ROLES = ["user", "admin", "moderator"];
 
 db.connect = function (then) {
     db.sequelize.sync({force: true}).then(() => {
         console.log('Drop and Resync Db');
-        pushData();
+        pushData(db);
 
         then();
     });
-
-    function pushData() {
-        db.role.create({
-            id: 1,
-            name: "user"
-        });
-
-        db.role.create({
-            id: 2,
-            name: "moderator"
-        });
-
-        db.role.create({
-            id: 3,
-            name: "admin"
-        });
-
-        db.user.create({
-            id: 1,
-            username: 'agata',
-            email: 'aga@best.pl',
-            password: 'A665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3'.toLowerCase()
-        }).then(user => {
-            user.addRole(1);
-        });
-
-        db.plant.create({
-            commonName: 'siberian pine',
-            binomialName: 'pinus sibirica',
-            kingdom: 'plantae',
-            family: 'pinaceae',
-            genus: 'pinus'
-        });
-
-        db.plant.create({
-            commonName: 'eastern white pine',
-            binomialName: 'pinus strobus',
-            kingdom: 'plantae',
-            family: 'pinaceae',
-            genus: 'Pinus'
-        });
-
-        db.plant.create({
-            commonName: 'swiss pine',
-            binomialName: 'pinus cembra',
-            kingdom: 'plantae',
-            family: 'pinaceae',
-            genus: 'pinus'
-        });
-
-        db.plant.create({
-            commonName: 'korean pine',
-            binomialName: 'pinus koraiensis',
-            kingdom: 'plantae',
-            family: 'pinaceae',
-            genus: 'pinus'
-        });
-
-        db.plant.create({
-            commonName: 'siberian dwarf pine',
-            binomialName: 'pinus pumila',
-            kingdom: 'plantae',
-            family: 'pinaceae',
-            genus: 'pinus'
-        });
-    }
 }
 
 module.exports = db;
---
+++ src/models/test.db.js
@@ -0,0 +1,70 @@
+module.exports = function pushData(db) {
+    db.role.create({
+        id: 1,
+        name: "user"
+    });
+
+    db.role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    db.role.create({
+        id: 3,
+        name: "admin"
+    });
+
+    db.user.create({
+        id: 1,
+        username: 'agata',
+        email: 'aga@best.pl',
+        password: 'A665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3'.toLowerCase(),
+        activated: true
+    }).then(user => {
+        user.addRole(1);
+        user.createProject({
+            name: 'First project',
+            description: 'Project created for testing purposes'
+        });
+    });
+
+    db.plant.create({
+        commonName: 'siberian pine',
+        binomialName: 'pinus sibirica',
+        kingdom: 'plantae',
+        family: 'pinaceae',
+        genus: 'pinus'
+    });
+
+    db.plant.create({
+        commonName: 'eastern white pine',
+        binomialName: 'pinus strobus',
+        kingdom: 'plantae',
+        family: 'pinaceae',
+        genus: 'Pinus'
+    });
+
+    db.plant.create({
+        commonName: 'swiss pine',
+        binomialName: 'pinus cembra',
+        kingdom: 'plantae',
+        family: 'pinaceae',
+        genus: 'pinus'
+    });
+
+    db.plant.create({
+        commonName: 'korean pine',
+        binomialName: 'pinus koraiensis',
+        kingdom: 'plantae',
+        family: 'pinaceae',
+        genus: 'pinus'
+    });
+
+    db.plant.create({
+        commonName: 'siberian dwarf pine',
+        binomialName: 'pinus pumila',
+        kingdom: 'plantae',
+        family: 'pinaceae',
+        genus: 'pinus'
+    });
+}
