55857b779c854dec6c16315d382b0adeadee9451
Przemysław Papla <przemekpapla@gmail.com>
2021-06-03 15:27:27 +0200

    Add plant layer

--- front/src/components/PlainEditorPage.tsx
+++ front/src/components/PlainEditorPage.tsx
@@ -9,11 +9,13 @@ import leaf from "../assets/leaf.svg";
 import tree from "../assets/tree.svg";
 import useImage from 'use-image';
 import {Button} from "react-bootstrap";
+import Plant from "./Plant";
 
 const PlainEditor = () => {
     const [image] = useImage(tree);
     const [points, setPoints] = useState([10, 10, 100, 100, 150, 50]);
     const [mode, setMode] = useState("move");
+    const [snap, setSnap] = useState(false);
 
     const lineRef = useRef<typeof Line>();
 
@@ -49,15 +51,34 @@ const PlainEditor = () => {
                              }
                              onDragEnd={(e) => {
                                  //console.log(e.target.attrs);
-
+                                 if (false) {
+                                     let x = e.target.attrs.x;
+                                     let y = e.target.attrs.y;
+                                     x = Math.floor(x / 10) * 10;
+                                     y = Math.floor(y / 10) * 10;
+                                     e.target.setPosition({x, y});
+                                 }
                                 }
                              }
                              onDragMove={(e) => {
                                  setPoints(points.map((p, j) => {
+                                     let x = e.target.attrs.x;
+                                     let y = e.target.attrs.y;
+
+                                     if (snap) {
+                                         // x = Math.floor(x / 10) * 10;
+                                         // y = Math.floor(y / 10) * 10;
+                                         if (snap) {
+                                             x = Math.round(x / 10) * 10;
+                                             y = Math.round(y / 10) * 10;
+                                             e.target.setPosition({x, y});
+                                         }
+                                     }
+
                                      if (j === i) {
-                                         return e.target.attrs.x;
+                                         return x;
                                      } else if (j === i+1) {
-                                         return e.target.attrs.y;
+                                         return y;
                                      } else {
                                          return p;
                                      }
@@ -82,19 +103,52 @@ const PlainEditor = () => {
         /> )
     }
 
+
+
+    const width = window.innerWidth;
+    const height = 500;
+
     const grids = [];
-    for (let r = 0; r < 8; r++) {
-        for (let c = 0; c < 6; c++) {
-            grids.push(<Rect x={r*100} y={c*100} width={100} height={100} strokeWidth={1} stroke={"black"}/>);
+    for (let r = 0; r < width / 100; r++) {
+        for (let c = 0; c < height / 100; c++) {
+            grids.push(<Rect x={r*100} y={c*100} width={100} height={100} strokeWidth={0.5} stroke={"gray"}/>);
         }
     }
+
+    const plant = {
+        x: 150,
+        y: 150,
+        width: 100,
+        height: 100,
+        id: 'rect2',
+    };
+
+    const [plants, setPlants] = React.useState([
+        plant
+    ]);
+    const [selectedId, selectPlant] = React.useState<any>(null);
+
+    const checkDeselect = (e: any) => {
+        // const clickedOnEmpty = e.target === e.target.getStage();
+        // console.log(e.target.getLayer());
+        const clickedOnEmpty = e.target.getLayer().attrs.name !== 'plants';
+        if (clickedOnEmpty) {
+            selectPlant(null);
+        }
+    };
 
     return <>
         <Button onClick={() => setMode("add")}>Add</Button>
         <Button onClick={() => setMode("move")}>Move</Button>
         <Button onClick={() => setMode("del")}>Remove</Button>
+        <Button onClick={() => setSnap(!snap)}>Snap</Button>
         {/*width={800} height={600}*/}
-        <Stage width={1800} height={500} onClick={(e) => {
+        <Stage
+            onMouseDown={checkDeselect}
+            onTouchStart={checkDeselect}
+            width={width}
+            height={height}
+            onClick={(e) => {
             if (mode === 'add') {
                 console.log(e);
                 //setPoints(points.concat([e.target.attrs.x, e.target.attrs.y]));
@@ -107,15 +161,34 @@ const PlainEditor = () => {
                     grids.map(grid => grid)
                 }
             </Layer>
-            <Layer>
-                <Image image={image} scale={{x: 0.5, y: 0.5}} draggable/>
+            <Layer name="plants">
+                {
+                    plants.map((plant, i) => (
+                        <Plant
+                            key={i}
+                            image={image}
+                            shapeProps={plant}
+                            isSelected={plant.id === selectedId}
+                            onSelect={() => {
+                                selectPlant(plant.id);
+                            }}
+                            onChange={(newAttrs: any) => {
+                                const newPlants = plants.slice();
+                                newPlants[i] = newAttrs;
+                                setPlants(newPlants);
+                            }}
+                        />
+                    ))
+                }
+
+                {/*<Image image={image} scale={{x: 0.5, y: 0.5}} draggable/>*/}
             </Layer>
             <Layer>
                 {/*@ts-ignore*/}
                 <Line ref={lineRef}
                     points={points}
-                    stroke={'red'}
-                    strokeWidth={15}
+                    stroke={'#650425'}
+                    strokeWidth={5}
                     //tension={0.01}
                 />
             </Layer>
---
+++ front/src/components/Plant.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {Image, Rect, Transformer} from "react-konva";
+
+const Plant = ({ shapeProps, isSelected, onSelect, onChange, image }: any) => {
+    const shapeRef = React.useRef<any>();
+    const trRef = React.useRef<any>();
+
+    React.useEffect(() => {
+        if (isSelected && trRef.current) {
+            trRef.current.nodes([shapeRef.current]);
+            trRef.current.getLayer().batchDraw();
+        }
+    }, [isSelected]);
+
+    return (
+        <React.Fragment>
+            <Image image={image}
+                onClick={onSelect}
+                onTap={onSelect}
+                ref={shapeRef}
+                {...shapeProps}
+                draggable
+                onDragEnd={(e) => {
+                    onChange({
+                        ...shapeProps,
+                        x: e.target.x(),
+                        y: e.target.y(),
+                    });
+                }}
+                onTransformEnd={(e) => {
+                    const node = shapeRef.current;
+                    if (node) {
+                        const scaleX = node.scaleX();
+                        const scaleY = node.scaleY();
+
+                        node.scaleX(1);
+                        node.scaleY(1);
+
+                        onChange({
+                            ...shapeProps,
+                            x: node.x(),
+                            y: node.y(),
+                            width: Math.max(5, node.width() * scaleX),
+                            height: Math.max(node.height() * scaleY),
+                        });
+                    }
+                }}
+            />
+            {isSelected && (
+                <Transformer
+                    ref={trRef}
+                    boundBoxFunc={(oldBox, newBox) => {
+                        if (newBox.width < 50 || newBox.height < 50) {
+                            return oldBox;
+                        } else if (newBox.width > 250 || newBox.height > 250) {
+                            return oldBox;
+                        }
+                        return newBox;
+                    }}
+                />
+            )}
+        </React.Fragment>
+    );
+};
+
+export default Plant;
